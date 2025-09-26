4c912fb17f4dc22a15ebe8ccba5d5262fde5924e
hugo <hugo.fcarvalho@sptech.school>
2025-09-26 19:56:25 -0300

    feat(itens de produto): adicionar dtos e documentação swagger

---
+++ EncantoPersonalizados/src/main/java/school/sptech/EncantoPersonalizados/dto/itemProduto/ItemProdutoMapper.java
@@ -0,0 +1,67 @@
+package school.sptech.EncantoPersonalizados.dto.itemProduto;
+
+import school.sptech.EncantoPersonalizados.entities.ItemProduto;
+import school.sptech.EncantoPersonalizados.service.ItemProdutoService;
+
+import java.util.List;
+
+public class ItemProdutoMapper {
+    public static ItemProduto toEntity(ItemProdutoRequestDTO dto){
+        if (dto == null) return null;
+        ItemProduto entity = new ItemProduto();
+        entity.setDescricao(dto.descricao());
+        entity.setPrecoVenda(dto.precoVenda());
+        entity.setPrazoProducao(dto.prazoProducao());
+        entity.setLargura(dto.largura());
+        entity.setAltura(dto.altura());
+        entity.setPeso(dto.peso());
+        entity.setComprimento(dto.comprimento());
+        entity.setMaterial(dto.material());
+        entity.setCustoProducao(dto.custoProducao());
+        return entity;
+
+    }
+
+    public static ItemProdutoResponseDTO toDto(ItemProduto entity){
+        if(entity == null) return null;
+        ItemProdutoResponseDTO dto = new ItemProdutoResponseDTO(
+                entity.getId(),
+                entity.getDescricao(),
+                entity.getPrecoVenda(),
+                entity.getCustoProducao(),
+                entity.getPrazoProducao(),
+                entity.getLargura(),
+                entity.getAltura(),
+                entity.getPeso(),
+                entity.getComprimento(),
+                entity.getMaterial(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
+        return dto;
+    }
+    public static List<ItemProdutoResponseDTO> toDto(List<ItemProduto> entities){
+        if(entities == null) return null;
+
+        return entities
+                .stream()
+                .map(entity -> {
+                    ItemProdutoResponseDTO dto = new ItemProdutoResponseDTO(
+                            entity.getId(),
+                            entity.getDescricao(),
+                            entity.getPrecoVenda(),
+                            entity.getCustoProducao(),
+                            entity.getPrazoProducao(),
+                            entity.getLargura(),
+                            entity.getAltura(),
+                            entity.getPeso(),
+                            entity.getComprimento(),
+                            entity.getMaterial(),
+                            entity.getCreatedAt(),
+                            entity.getUpdatedAt()
+                    );
+                    return dto;
+                })
+                .toList();
+    }
+}
---
+++ EncantoPersonalizados/src/main/java/school/sptech/EncantoPersonalizados/dto/itemProduto/ItemProdutoResponseDTO.java
@@ -0,0 +1,32 @@
+package school.sptech.EncantoPersonalizados.dto.itemProduto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+public record ItemProdutoResponseDTO(
+        Integer id,
+        @Schema(description = "Nome do produto")
+         String descricao,
+        @Schema(description = "Double do preço que o produto será vendido")
+         Double precoVenda,
+        @Schema(description = "Double do custo de produção do produto")
+         Double custoProducao,
+        @Schema(description = "Prazo emd dias para produção do produto")
+         Integer prazoProducao,
+        @Schema(description = "Largura em cm do produto")
+         Double largura,
+        @Schema(description = "Altura em cm do produto")
+         Double altura,
+        @Schema(description = "Peso em g do produto")
+         Double peso,
+        @Schema(description = "Comprimento em cm do produto")
+         Double comprimento,
+        @Schema(description = "Materia prima do produto ")
+         String material,
+        @Schema(description = "Data da criação do usuário")
+        LocalDateTime createdAt,
+        @Schema(description = "Data da última atualização do usuário")
+        LocalDateTime updatedAt
+) {
+}
