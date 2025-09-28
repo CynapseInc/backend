60029c670ff887b2ba94f2f1fc82d77158c3f1d4
DYTateno <diogo.tateno@sptech.school>
2025-09-28 20:47:41 -0300

    chore: documentação mapper no controller ItemProduto

--- EncantoPersonalizados/src/main/java/school/sptech/EncantoPersonalizados/controller/ItemProdutoController.java
+++ EncantoPersonalizados/src/main/java/school/sptech/EncantoPersonalizados/controller/ItemProdutoController.java
@@ -1,11 +1,18 @@
 package school.sptech.EncantoPersonalizados.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.coyote.Response;
 import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import school.sptech.EncantoPersonalizados.dto.itemProduto.ItemProdutoRequestDTO;
 import school.sptech.EncantoPersonalizados.dto.itemProduto.ItemProdutoResponseDTO;
+import school.sptech.EncantoPersonalizados.dto.usuario.UsuarioResponseDTO;
 import school.sptech.EncantoPersonalizados.entities.ItemProduto;
 import school.sptech.EncantoPersonalizados.entities.Usuario;
 import school.sptech.EncantoPersonalizados.service.ItemProdutoService;
@@ -22,12 +29,22 @@ public class ItemProdutoController {
         this.service = service;
     }
 
+    @Operation(description = "Cadastra um produto")
+    @ApiResponses(
+            @ApiResponse(responseCode = "201", description = "Retorna o produto")
+    )
+
     @PostMapping
     public ResponseEntity<ItemProdutoResponseDTO> cadastrar(@RequestBody ItemProdutoRequestDTO item){
         ItemProdutoResponseDTO ProdutoRegistrado = service.cadastrar(item);
         return ResponseEntity.status(201).body(ProdutoRegistrado);
     }
 
+    @Operation(description = "Lista todos os produtos")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Retorna a lista de produtos vazia"),
+            @ApiResponse(responseCode = "204", description = "Sucesso ao listar, sem conteúdo")
+    })
     @GetMapping
     public ResponseEntity<List<ItemProdutoResponseDTO>> listar(){
         List<ItemProdutoResponseDTO> itens = service.listar();
@@ -37,6 +54,11 @@ public class ItemProdutoController {
         return ResponseEntity.status(204).build();
     }
 
+    @Operation(description = "Lista produtos menor do que o valor especificado")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Retorna a lista de produtos vazia"),
+            @ApiResponse(responseCode = "204", description = "Sucesso ao listar, sem conteúdo")
+    })
     @GetMapping("/{preco}")
     public ResponseEntity<List<ItemProdutoResponseDTO>> listarPrecoMenorQue(@PathVariable Double preco){
         List<ItemProdutoResponseDTO> itensPrecoMenosQue = service.buscarPorPrecoMenorQue(preco);
@@ -46,14 +68,28 @@ public class ItemProdutoController {
         return ResponseEntity.status(204).build();
     }
 
+    @Operation(description = "Atualiza produto")
+    @ApiResponses(
+            @ApiResponse(responseCode = "200", description = "Retorna produto atualizado")
+    )
     @PutMapping("/{id}")
-    public ResponseEntity<ItemProdutoResponseDTO> updatePorId(@RequestBody ItemProduto item, @PathVariable Long id){
+    public ResponseEntity<ItemProdutoResponseDTO> updatePorId(
+            @RequestBody ItemProduto item,
+
+            @Parameter(description = "Id do produto", example = "1", required = true)
+            @PathVariable Long id
+    ){
         ItemProdutoResponseDTO itemAtualizado = service.update(id, item);
         return ResponseEntity.status(200).body(itemAtualizado);
      }
 
+     @Operation(description = "Exclui produto")
+     @ApiResponses(@ApiResponse(responseCode = "204", description = "Produto excluído, sem conteúdo"))
     @DeleteMapping("/{id}")
-     public ResponseEntity<Void> excluirPorId(@PathVariable Long id){
+     public ResponseEntity<Void> excluirPorId(
+            @Parameter(description = "Id do produto", example = "1", required = true)
+            @PathVariable Long id
+    ){
          service.delete(id);
          return ResponseEntity.status(204).build();
      }
